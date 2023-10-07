0a52850bcf75bae66d1cbe5f3f17fd30f0c4cd72
Kamran Wali <kamran369mars@gmail.com>
2023-10-07 19:06:04 +0100

    Update cop_variable_creator_plugin.gd
    Commented out the method  which is never called. This zombie code must  be removed soon for avoiding mistakes.

--- addons/cop_variable_creator/cop_variable_creator_plugin.gd
+++ addons/cop_variable_creator/cop_variable_creator_plugin.gd
@@ -6,12 +6,10 @@ var _dock
 func _enter_tree():
 	_dock = preload("res://addons/cop_variable_creator/variable_creator.tscn").instantiate()
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, _dock)
-	pass
-
 
 func _exit_tree():
 	remove_control_from_docks(_dock)
 	_dock.free()
 
-func _scan_file_system() -> void:
-	self.get_editor_interface().get_resource_filesystem().scan()
+#func _scan_file_system() -> void:
+#	self.get_editor_interface().get_resource_filesystem().scan()
