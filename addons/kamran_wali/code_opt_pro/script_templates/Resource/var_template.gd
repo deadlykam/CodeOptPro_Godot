938a995a8527f878f68d098b563c15f288260b2c
Kamran Wali <kamran369mars@gmail.com>
2023-10-08 18:17:02 +0100

    Folder refactor done

---
+++ addons/kamran_wali/code_opt_pro/script_templates/Resource/var_template.gd
@@ -0,0 +1,15 @@
+#class_name COP_SomeVar
+extends Resource
+
+# Set the type of the _value for better performance.
+var _value
+
+# Set the return type for better performance.
+## This method gets the value.
+func get_value():
+	return _value
+	
+# Set the parameter type for better performance.
+## This method sets the value.
+func set_value(value):
+	_value = value
