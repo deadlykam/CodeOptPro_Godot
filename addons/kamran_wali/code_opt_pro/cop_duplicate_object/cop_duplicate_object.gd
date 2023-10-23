dfe0512ddd746efb35dc28b9d3b07bff7d58a2a8
Kamran Wali <kamran369mars@gmail.com>
2023-10-23 19:13:47 +0100

    Refactored To Instantiate Object

--- addons/kamran_wali/code_opt_pro/cop_duplicate_object/cop_duplicate_object.gd
+++ addons/kamran_wali/code_opt_pro/cop_duplicate_object/cop_duplicate_object.gd
@@ -1,16 +1,13 @@
 @tool
 extends "res://addons/kamran_wali/code_opt_pro/scripts/code_opt_pros/base_plugin.gd"
 
-const _FROM_FILES = "res://"
 const _FROM_TSCN = ".tscn"
 
 # Properties from the scene.
-var _object_path_txt: LineEdit
-var _object_status: Label
 var _parent_status_lbl: Label
 var _scene_status_lbl: Label
-var _num_duplicate: LineEdit
-var _duplicate_button: CanvasItem
+var _num_instantiate: LineEdit
+var _instantiate_button: CanvasItem
 var _parent_lock_button: CheckButton
 var _scene_lock_button: CheckButton
 
@@ -18,34 +15,30 @@ var _scene_lock_button: CheckButton
 var _parent_node: Node
 var _scene_object: PackedScene
 var _temp_object
-var _object
-var _object_dup
+var _object_instantiate
 var _index: int
 var _counter: int
+var _index_name: int
+var _counter_name: int
 var _is_parent: bool
 var _is_scene: bool
-var _is_object: bool
 var _is_number: bool
 
 func _enter_tree() -> void:
 	# Setting up the scene variables
-	_object_path_txt = $MainContainer/ObjectContainer/ObjectPath_Txt
-	_object_status = $MainContainer/ObjectContainer/ObjectStatus
 	_parent_status_lbl = $MainContainer/ParentContainer/ParentStatus
-	_scene_lock_button = $MainContainer/SceneContainer/SceneLockButton
 	_scene_status_lbl = $MainContainer/SceneContainer/SceneStatusLabel
-	_num_duplicate = $MainContainer/NumberContainer/Input_Number
-	_duplicate_button = $MainContainer/DuplicateButton
+	_num_instantiate = $MainContainer/NumberContainer/Input_Number
+	_instantiate_button = $MainContainer/InstantiateButton
 	_parent_lock_button = $MainContainer/ParentContainer/LockButton
-	set_control_font_colour(_object_status, Color.INDIAN_RED) # Setting the colour at start.
+	_scene_lock_button = $MainContainer/SceneContainer/SceneLockButton
 
 func _is_var_init() -> bool:
-	return (_object_path_txt != null && _object_status != null && _parent_status_lbl != null
-		&& _scene_lock_button != null && _scene_status_lbl != null && _num_duplicate != null
-		&& _duplicate_button != null && _parent_lock_button != null)
+	return (_parent_status_lbl != null && _scene_lock_button != null && _scene_status_lbl != null 
+		&& _num_instantiate != null && _instantiate_button != null && _parent_lock_button != null)
 
 func update(delta: float) -> void:
-	if _is_var_init():
+	if _is_var_init(): # Checking if all the variables are done loading up
 		if _is_parent_selected(): # Checking if parent has been selected.
 			# Checking if new parent has been selected and if locked button is disabled.
 			if _parent_node != _get_selected_parent_node() && !_parent_lock_button.button_pressed:
@@ -60,73 +53,69 @@ func update(delta: float) -> void:
 			set_control_font_colour(_parent_status_lbl, Color.INDIAN_RED)
 			_is_parent = false # Parent NOT selected
 		
-		if _is_scene_selected() && _scene_lock_button:
-			_temp_object = _get_selected_scene_object()
-			if _temp_object is PackedScene: # Checking if object scene object.
-				if _scene_object != _temp_object && !_scene_lock_button.button_pressed:
-					_scene_object = _temp_object
-					
-					if _scene_object != null:
-						_scene_status_lbl.set_text(_scene_object.resource_name)
-						set_control_font_colour(_scene_status_lbl, Color.GREEN)
-						_is_scene = true
-			else:
-				_scene_status_lbl.set_text("Scene object NOT selected.")
-			set_control_font_colour(_scene_status_lbl, Color.INDIAN_RED)
-		else:
-			_scene_status_lbl.set_text("Please select a scene objet from FileSystem.")
-			set_control_font_colour(_scene_status_lbl, Color.INDIAN_RED)
-			_is_scene = false
+		if !_scene_lock_button.button_pressed: # Condition for updating the scene object selection
+			if _is_filesystem_object_selected(): # Checking if filesystem object has been selected
+				_temp_object = _get_selected_filesystem_object() # Getting the selected filesystem object
+				
+				if _temp_object is PackedScene: # Checking if filesystem object is scene object.
+					if _scene_object != _temp_object: # Condition for updating the scene object
+						_scene_object = _temp_object # Updating the scene object
+						
+						if _scene_object != null: # Condition for updating scene object's status
+							_scene_status_lbl.set_text(_get_scene_object_name(_scene_object.resource_path))
+							set_control_font_colour(_scene_status_lbl, Color.GREEN)
+							_is_scene = true # Scene selected
+				else: # Condition for NOT selected scene object
+					_scene_status_lbl.set_text("Scene object NOT selected.")
+					set_control_font_colour(_scene_status_lbl, Color.INDIAN_RED)
+					_scene_object = null # Making null so later can select the previous object.
+					_is_scene = false # Scene NOT selected
+			else: # Condition for filesystem object NOT selected
+				_scene_status_lbl.set_text("Please select a scene object from FileSystem.")
+				set_control_font_colour(_scene_status_lbl, Color.INDIAN_RED)
+				_is_scene = false # Scene NOT selected
 		
 		if _is_show_button(): # Showing the button.
-			_duplicate_button.show()
+			_instantiate_button.show()
 		else: # Hiding the button.
-			_duplicate_button.hide()
-		
-	#	if Input.is_action_just_pressed("ui_page_up"):
-	#		if !EDITOR_PLUGIN.get_editor_interface().get_selected_paths().is_empty():
-	#			var object = load(EDITOR_PLUGIN.get_editor_interface().get_selected_paths()[0])
-	#			print("Path: " , EDITOR_PLUGIN.get_editor_interface().get_selected_paths()[0])
-	#			print("Path Object is PackedScene: " , object is PackedScene)
-
-func _on_object_path_txt_text_changed(path: String):
-	if _is_object_exist(path): # Checking if the object exists.
-		_object_status.set_text("Object Found!")
-		set_control_font_colour(_object_status, Color.GREEN)
-		_is_object = true # Object selected
-	else: # Object does NOT exist.
-		_object_status.set_text("Object NOT Found!")
-		set_control_font_colour(_object_status, Color.INDIAN_RED)
-		_is_object = false # Object NOT selected
+			_instantiate_button.hide()
 
 func _on_input_number_text_changed(value: String):
-	_is_number = is_int(value)
-	if _is_number:
-		set_control_font_colour(_num_duplicate, Color.WHITE)
+	if is_int(value) && value.to_int() > 0:
+		set_control_font_colour(_num_instantiate, Color.WHITE)
+		_is_number = true
 	else:
-		set_control_font_colour(_num_duplicate, Color.INDIAN_RED)
+		set_control_font_colour(_num_instantiate, Color.INDIAN_RED)
+		_is_number = false
 
 func _on_duplicate_button_pressed():
-	_counter = _num_duplicate.get_text().to_int() # Getting the counter value.
-	
+#	_counter = _num_duplicate.get_text().to_int() # Getting the counter value.
+#
+#	for _index in range(0, _counter): # Loop for creating objects.
+#		if _is_packedscene_object(): # Condition for duplicating PackedScenes.
+#			_object = load(_object_path_txt.get_text())
+#			_object_dup = _object.instantiate()
+#		else: # Condition for duplicating Scene Nodes.
+#			_object = get_scene_root_node().get_node(_object_path_txt.get_text())
+#			_object_dup = _object.duplicate()
+#
+#		_parent_node.add_child(_object_dup, true) # Adding object and making name readable.
+#		_object_dup.set_owner(get_scene_root_node()) # Setting the ownership to the correct scene.
+
+	_counter = _num_instantiate.get_text().to_int() if _is_number else 1 # Getting the counter value
+
 	for _index in range(0, _counter): # Loop for creating objects.
-		if _is_packedscene_object(): # Condition for duplicating PackedScenes.
-			_object = load(_object_path_txt.get_text())
-			_object_dup = _object.instantiate()
-		else: # Condition for duplicating Scene Nodes.
-			_object = get_scene_root_node().get_node(_object_path_txt.get_text())
-			_object_dup = _object.duplicate()
-		
-		_parent_node.add_child(_object_dup, true) # Adding object and making name readable.
-		_object_dup.set_owner(get_scene_root_node()) # Setting the ownership to the correct scene.
+		_object_instantiate = _scene_object.instantiate()
+		_parent_node.add_child(_object_instantiate, true) # Adding object and making name readable.
+		_object_instantiate.set_owner(get_scene_root_node()) # Setting the ownership to the correct scene.	
 
 ## This method checks if the a parent object has been selected.
 func _is_parent_selected() -> bool:
 	return (EDITOR_PLUGIN.get_editor_interface().get_selection().get_selected_nodes().size() != 0
 		|| _parent_node != null)
 
-## This method checks if the scene has been selected.
-func _is_scene_selected() -> bool:
+## This method checks if any file system object has been selected.
+func _is_filesystem_object_selected() -> bool:
 	return (EDITOR_PLUGIN.get_editor_interface().get_selected_paths().size() != 0
 		|| _scene_object != null)
 
@@ -137,8 +126,8 @@ func _get_selected_parent_node() -> Node:
 	
 	return EDITOR_PLUGIN.get_editor_interface().get_selection().get_selected_nodes()[0]
 
-## This method gets the selected scene object.
-func _get_selected_scene_object():
+## This method gets the selected filesystem object.
+func _get_selected_filesystem_object():
 	if (EDITOR_PLUGIN.get_editor_interface().get_selected_paths().size() != 0 &&
 		EDITOR_PLUGIN.get_editor_interface().get_selected_paths()[0].substr(
 			EDITOR_PLUGIN.get_editor_interface().get_selected_paths()[0].length() - _FROM_TSCN.length(), 
@@ -152,12 +141,12 @@ func _is_object_exist(path: String) -> bool:
 
 ## This method checks if to show the button.
 func _is_show_button() -> bool:
-	return _is_parent && _is_object && _is_number
+	return _is_parent && _is_scene
 
-## This method checks if the object given is a packed scene or NOT.
-func _is_packedscene_object() -> bool:
-	if _object_path_txt.get_text().length() >= _FROM_FILES.length():
-		if _object_path_txt.get_text().substr(0, _FROM_FILES.length()).contains(_FROM_FILES):
-			return true
-	
-	return false
+## This method gets the name of the scene object.
+func _get_scene_object_name(name: String) -> String:
+	for _counter_name in range(name.length() - 1, -1, -1): # Loop for finding the name
+		if name[_counter_name] == "/": # Condition for breaking the loop
+			_index_name = _counter_name + 1 # Start index of the sub-string
+			break
+	return name.substr(_index_name, name.length()) # Returning the actual name sub-string
