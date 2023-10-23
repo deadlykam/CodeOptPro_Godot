780fe15afc29deccd47460cacee2fdf43bdbfbce
Kamran Wali <kamran369mars@gmail.com>
2023-10-23 20:05:45 +0100

    Renamed plugin

--- addons/kamran_wali/code_opt_pro/cop_instantiate_object/cop_instantiate_object_plugin.gd
+++ addons/kamran_wali/code_opt_pro/cop_instantiate_object/cop_instantiate_object_plugin.gd
@@ -1,13 +1,13 @@
 @tool
 extends EditorPlugin
 
-const COP_DUPLICATE_OBJECT: GDScript = preload("res://addons/kamran_wali/code_opt_pro/cop_duplicate_object/cop_duplicate_object.gd")
+const COP_DUPLICATE_OBJECT: GDScript = preload("res://addons/kamran_wali/code_opt_pro/cop_instantiate_object/cop_instantiate_object.gd")
 
 var _dock
 
 func _enter_tree():
 	COP_DUPLICATE_OBJECT.set_plugin(self) # Setting the plugin.
-	_dock = preload("res://addons/kamran_wali/code_opt_pro/cop_duplicate_object/duplicate_object.tscn").instantiate()
+	_dock = preload("res://addons/kamran_wali/code_opt_pro/cop_instantiate_object/instantiate_object.tscn").instantiate()
 	add_control_to_dock(DOCK_SLOT_RIGHT_BL, _dock)
 
 func _exit_tree():
