48a874520315a8580a037d021764424dd19455ac
Kamran Wali <kamran369mars@gmail.com>
2023-11-21 20:08:28 +0000

    Fixed types and removed similar methods

--- addons/kamran_wali/code_opt_pro/scripts/bars/base_bar.gd
+++ addons/kamran_wali/code_opt_pro/scripts/bars/base_bar.gd
@@ -1,16 +1,16 @@
 class_name COP_BaseBar
 extends Node
 
-var _value_max: int
-var _value_cur: int
+var _value_max:= 1
+var _value_cur:= 0
 
 ## This method sets the max value.
 func set_max(value: int) -> void:
-	_value_max = value
+	_value_max = value if value > 0 else 1
 
-## This method sets the current value.
+## This method sets the current value with the given value, limit is 0 to max value.
 func set_current(value: int) -> void:
-	_value_cur = value
+	_value_cur = 0 if value < 0 else _value_max if value >= _value_max else value
 
 ## Gets the normal value of the bar
 func get_normal() -> float:
@@ -20,16 +20,12 @@ func get_normal() -> float:
 func add(value: int) -> void:
 	_value_cur = _value_cur + value if (_value_cur + value) < _value_max else _value_max
 
-## This method sets the current value with the given value, limit is 0 to max value.
-func set_value_current(value: int) -> void:
-	_value_cur = 0 if value < 0 else _value_max if value >= _value_max else value
-
 ## This method gets the max value of the bar.
-func get_value_max() -> float:
+func get_value_max() -> int:
 	return _value_max
 
 ## This method gets the current value of the bar.
-func get_value_current() -> float:
+func get_value_current() -> int:
 	return _value_cur
 
 ## This method checks if the bar is full.
